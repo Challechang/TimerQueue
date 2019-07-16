d22b972848988e1fb8265f1fc0fce8864e993de4
rbcheng <rbcheng@qq.com>
2019-07-16 19:30:26 +0800

    update cmake

--- TimerQueue/timer_queue_test.cpp
+++ TimerQueue/timer_queue_test.cpp
@@ -1,31 +1,31 @@
-#include <chrono>
-#include <iostream>
-#include "Logger.hpp"
-#include "TimerQueue.hpp"
-
-void test()
-{
-
-  LOG_DEBUG << "[test] : test timerQue happended ";
-
-  std::cout << "[test] : test timerQue happended at " << std::chrono::system_clock::now().time_since_epoch() / std::chrono::microseconds(1) << std::endl;
-
-}
-
-
-int main()
-{
-
-  //Logger::setLogLevel(Logger::TRACE);
-
-  TimerQueue* timer_queue = TimerQueue::GetInstance();
-  timer_queue->Start();
-  timer_queue->runAfter(1.0, test);
-  timer_queue->runAfter(1.0, test);
-  timer_queue->runAfter(3.0, test);
-
-  timer_queue->runEvery(5.0, test);
-  getchar();
-  return 0;
-}
-
+//#include <chrono>
+//#include <iostream>
+//#include "Logger.hpp"
+//#include "TimerQueue.hpp"
+//
+//void test()
+//{
+//
+//  LOG_DEBUG << "[test] : test timerQue happended ";
+//
+//  std::cout << "[test] : test timerQue happended at " << std::chrono::system_clock::now().time_since_epoch() / std::chrono::microseconds(1) << std::endl;
+//
+//}
+//
+//
+//int main()
+//{
+//
+//  //Logger::setLogLevel(Logger::TRACE);
+//
+//  TimerQueue* timer_queue = TimerQueue::GetInstance();
+//  timer_queue->Start();
+//  timer_queue->runAfter(1.0, test);
+//  timer_queue->runAfter(1.0, test);
+//  timer_queue->runAfter(3.0, test);
+//
+//  timer_queue->runEvery(5.0, test);
+//  getchar();
+//  return 0;
+//}
+//
